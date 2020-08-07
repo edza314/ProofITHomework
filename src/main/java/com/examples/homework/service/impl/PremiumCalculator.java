5a1c9f34935bcd1dcb678ee59255920b1e7d0071
edza314 <e.alksnis7@gmail.com>
2020-08-07 11:14:33 +0300

    Strategies update

--- src/main/java/com/examples/homework/service/impl/PremiumCalculator.java
+++ src/main/java/com/examples/homework/service/impl/PremiumCalculator.java
@@ -4,42 +4,50 @@ import com.examples.homework.controller.api.PolicyRequest;
 import com.examples.homework.controller.api.PolicyResponse;
 import com.examples.homework.controller.api.PolicySubObject;
 import com.examples.homework.service.Calculator;
-import com.examples.homework.strategies.Strategy;
 import com.examples.homework.strategies.impl.FireStrategy;
 import com.examples.homework.strategies.impl.TheftStrategy;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
 public class PremiumCalculator implements Calculator {
+
+    private List<Function<List<PolicySubObject>, BigDecimal>> allStrategies = new ArrayList<>();
+
+    {
+        allStrategies.add(new FireStrategy()::calculateValue);
+        allStrategies.add(new TheftStrategy()::calculateValue);
+    }
 
     @Override
     public PolicyResponse calculate(PolicyRequest request) {
 
         PolicyResponse response = new PolicyResponse();
         try {
+            List<PolicySubObject> policySubObjects = getPolicySubObjectsListFromRequest(request);
 
-            List<PolicySubObject> policySubObjects =
-                    request.getPolicy().getPolicyObjects()
-                            .stream()
-                            .filter(i -> i.getPolicySubObjects().size() > 0)
-                            .flatMap(i -> i.getPolicySubObjects().stream())
-                            .collect(Collectors.toList());
+            BigDecimal premium = allStrategies.stream()
+                    .map(i -> i.apply(policySubObjects))
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-            Strategy strategy = new FireStrategy();
-            BigDecimal premiumFire = strategy.calculateValue(policySubObjects);
-            strategy = new TheftStrategy();
-            BigDecimal premiumTheft = strategy.calculateValue(policySubObjects);
-
-            response.setPremium(premiumFire.add(premiumTheft));
+            response.setPremium(premium);
         } catch (Exception e) {
             e.getMessage();
         }
 
         return response;
+    }
+
+    private List<PolicySubObject> getPolicySubObjectsListFromRequest(PolicyRequest request) {
+        return request.getPolicy().getPolicyObjects()
+                .stream()
+                .filter(i -> i.getPolicySubObjects().size() > 0)
+                .flatMap(i -> i.getPolicySubObjects().stream())
+                .collect(Collectors.toList());
     }
 }
